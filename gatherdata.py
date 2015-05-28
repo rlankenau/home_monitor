c6dda681c5930df1b74c7591efca93cd53d5e0ad
Russ Lankenau <rlankenau@gmail.com>
2015-05-28 07:21:56 -0500

    Changed connection management.

--- gatherdata.py
+++ gatherdata.py
@@ -1,17 +1,18 @@
 import serial
 import httplib 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-thingspeak = httplib.HTTPSConnection('api.thingspeak.com')
 
 while True:
     line = ser.readline()
     if line:
         print line
         try:
+            thingspeak = httplib.HTTPSConnection('api.thingspeak.com')
             thingspeak.request("GET", '/update?key=LNCS1U0RV05E9XH7&' + line)
             res = thingspeak.getresponse()
             print res.status, res.reason
             data = res.read()
+            thingspeak.close();
             print data
         except httplib.BadStatusLine as e:
             print "Server returned something we didn't understand: " + e.__str__()
