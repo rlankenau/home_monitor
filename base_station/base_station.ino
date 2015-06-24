05af51aa7f9adf6fe0e99d23ee1c7aad23cbc2cf
Russ Lankenau <rlankenau@gmail.com>
2015-06-24 11:18:58 -0500

    Updated with first cut at radio code.

--- base_station/base_station.ino
+++ base_station/base_station.ino
@@ -1,8 +1,6 @@
 #include <SPI.h>
 
-#include <RHSPIDriver.h>
 #include <RH_NRF24.h>
-#include <RH_ASK.h>
 #include <RadioHead.h>
 
 
@@ -15,22 +13,65 @@
 DHT dht(SENSOR, SENSOR_TYPE);
 /* TODO: Move key to EEPROM and have each module read it from there. */
 
+#define AGGREGATOR_ADDRESS 255
+
+RH_NRF24 driver(8,9);
+
 void setup()
 {
   dht.begin();
   Serial.begin(9600);
+  
+  if(!driver.init())
+  {
+    Serial.println("radio init failed.");
+    while(1)
+    {
+
+    }
+  }
+  
+  Serial.println("Starting main loop.");
 }
+
+typedef struct sensor_msg_t_stct {
+  float humidity;
+  float tempF;
+  float heatIndex;
+} sensor_msg_t;
+
+sensor_msg_t msg;
+long delta=0;
 
 void loop()
 {
-  /* Sleep for 1 minute. */
-  delay(60000);
-  float h = dht.readHumidity();
-  float t = dht.readTemperature(true);
-  float hi = dht.computeHeatIndex(t, h);
+  long loop_start = millis();
   
-  Serial.print("field1=");
-  Serial.print(h);
-  Serial.print("&field2=");
-  Serial.print(t);
+  uint8_t len = sizeof(sensor_msg_t);
+  /* Just spin on the radio, checking for available data. */
+  if(driver.available()) { 
+    driver.recv((uint8_t *)&msg, &len);
+    Serial.print(" humidity=");
+    Serial.print(msg.humidity);
+    Serial.print(" tempF=");
+    Serial.print(msg.tempF);
+    
+  } 
+ 
+  delta = millis() - loop_start + 1;
+  
+  /* If we rack up more than 30 seconds servicing others, do a read
+  for ourselves. */
+  if(delta > 30000) {
+    delta = 0;
+    float h = dht.readHumidity();
+    float t = dht.readTemperature(true);
+    float hi = dht.computeHeatIndex(t, h);
+  
+    Serial.print("origin=255");
+    Serial.print(" humidity=");
+    Serial.print(h);
+    Serial.print(" tempF=");
+    Serial.print(t);
+  }
 }
--- sensor_module/sensor_module.ino
+++ sensor_module/sensor_module.ino
@@ -1,6 +1,5 @@
 #include <SPI.h>
 
-#include <RHMesh.h>
 #include <RH_NRF24.h>
 #include <RadioHead.h>
 
@@ -18,21 +17,20 @@ DHT dht(SENSOR, SENSOR_TYPE);
 #define CLIENT_ADDRESS 1
 #define AGGREGATOR_ADDRESS 255
 
-RH_NRF24 driver;
-
-RHMesh manager(driver, CLIENT_ADDRESS);
+RH_NRF24 driver(8,9);
 
 void setup()
 {
-  pinMode(13,OUTPUT);
+
   dht.begin();
   Serial.begin(9600);
 
-  if(!manager.init())
+  if(!driver.init())
   {
-    Serial.println("init failed!");
+    Serial.println("radio init failed.");
+    while(1){}
   }
-
+  Serial.println("Starting main loop.");
 }
 
 typedef struct sensor_msg_t_stct {
@@ -43,13 +41,6 @@ typedef struct sensor_msg_t_stct {
 
 sensor_msg_t msg;
 
-void indicate_error()
-{
-  digitalWrite(13,HIGH);
-  delay(100);
-  digitalWrite(13,LOW);
-}
-
 void loop()
 {
   /* Sleep for 1 minute. */
@@ -57,10 +48,10 @@ void loop()
   msg.tempF = dht.readTemperature(true);
   msg.heatIndex = dht.computeHeatIndex(msg.tempF, msg.humidity);
  
-  if(manager.sendtoWait((uint8_t *)&msg, sizeof(sensor_msg_t), AGGREGATOR_ADDRESS) == RH_ROUTER_ERROR_NONE)
-  {
-    indicate_error();
+  for(int i=0;i<4;i++) {
+    driver.send((uint8_t *)&msg, sizeof(sensor_msg_t));
+    delay(random(10,30));
   }
   
-  delay(60000);
+  delay(500);
 }
